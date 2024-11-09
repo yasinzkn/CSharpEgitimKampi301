a512d8e149a6fe6e3ad13b195eb0a9b677a78bc3
yasin <yasin@LAPTOP-8VQTI9O3>
2024-11-09 21:15:15 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
+
+/*
+ Field-Variable-Property 
+*/
+/*
+int x; -> Field --> Bir sınıfın içinde tanımlandıysa -> Field
+Sınıf içerisinde get ve set ile tanımlanırsa -> Property
+Bir metot içinde tanımlanırsa --> Variable
+*/
---
+++ CSharpEgitimKampi301.EntityLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpEgitimKampi301.EntityLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("CSharpEgitimKampi301.EntityLayer")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42e1385b-8e20-4657-a344-ad80559f6a08")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
